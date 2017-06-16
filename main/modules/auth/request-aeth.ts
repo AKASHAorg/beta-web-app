d97defad857bcf437ea12e7c9b5354d1e8e5a33b
Marius Darila <marius.darila@gmail.com>
2017-06-16 19:42:06 +0300

    refactor(): add conf for web

--- main/modules/auth/request-aeth.ts
+++ main/modules/auth/request-aeth.ts
@@ -1,15 +1,17 @@
 import * as Promise from 'bluebird';
-import { post as POST } from 'request';
+import { post as POST } from 'superagent';
 import { FAUCET_TOKEN, FAUCET_URL } from '../../config/settings';
 
 const execute = Promise.coroutine(function*(data: RequestEtherRequest) {
     return Promise.fromCallback(function (cb) {
-        return POST({
-            url: FAUCET_URL,
-            json: { address: data.address, token: FAUCET_TOKEN },
-            agentOptions: { rejectUnauthorized: false }
-        }, cb);
-    }, { multiArgs: true }).spread(function (response, body) {
+        return POST(FAUCET_URL)
+            .set('Content-Type', 'application/json')
+            .send({ address: data.address, token: FAUCET_TOKEN })
+            .end(cb);
+    }).then((body) => {
+        if (body.ok && body.text) {
+            return JSON.parse(body.text);
+        }
         return body;
     });
 });
