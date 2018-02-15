3a64529ea5cc94a03f3ea76f0f7967fb1d6bad20
Marius Darila <marius.darila@gmail.com>
2018-02-15 18:46:10 +0200

    chore(): sync main updates

--- main/modules/profile/profile-data.ts
+++ main/modules/profile/profile-data.ts
@@ -29,7 +29,7 @@ export const getProfileData = {
  * Get profile data for an akasha profile address
  * @type {Function}
  */
-const execute = Promise.coroutine(function* (data: ProfileDataRequest) {
+const execute = Promise.coroutine(function* (data: ProfileDataRequest, cb) {
     const v = new schema.Validator();
     v.validate(data, getProfileData, { throwError: true });
 
@@ -48,6 +48,19 @@ const execute = Promise.coroutine(function* (data: ProfileDataRequest) {
     const entriesCount = yield entryCountProfile.execute({ ethAddress });
     const commentsCount = yield contracts.instance.Comments.totalCommentsOf(ethAddress);
     const [karma, essence] = yield contracts.instance.Essence.getCollected(ethAddress);
+    const partialProfile = {
+        akashaId: akashaId,
+        ethAddress: ethAddress,
+        donationsEnabled: donationsEnabled,
+        followingCount: foCount.count,
+        followersCount: fwCount.count,
+        entriesCount: entriesCount.count,
+        commentsCount: commentsCount.toString(10),
+        [BASE_URL]: generalSettings.get(BASE_URL),
+        karma: (web3Api.instance.fromWei(karma, 'ether')).toFormat(5),
+        essence: (web3Api.instance.fromWei(essence, 'ether')).toFormat(5)
+    };
+    cb('', partialProfile);
     if (!!unpad(hash)) {
         const ipfsHash = encodeHash(fn, digestSize, hash);
         if (data.short) {
@@ -69,21 +82,8 @@ const execute = Promise.coroutine(function* (data: ProfileDataRequest) {
         }], (err) => { if (err) { console.warn('error storing PROFILE index', err); } });
     }
 
-    return Object.assign(
-        {
-            akashaId: akashaId,
-            ethAddress: ethAddress,
-            donationsEnabled: donationsEnabled,
-            followingCount: foCount.count,
-            followersCount: fwCount.count,
-            entriesCount: entriesCount.count,
-            commentsCount: commentsCount.toString(10),
-            [BASE_URL]: generalSettings.get(BASE_URL),
-            karma: (web3Api.instance.fromWei(karma, 'ether')).toFormat(5),
-            essence: (web3Api.instance.fromWei(essence, 'ether')).toFormat(5)
-        },
-        profile);
+    return Object.assign({}, partialProfile, profile);
 
 });
 
-export default { execute, name: 'getProfileData' };
+export default { execute: execute, name: 'getProfileData', hasStream: true };
