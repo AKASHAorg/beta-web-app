15c56cf373c5366ec8b0f6282eb0a729538ca604
Marius Darila <marius.darila@gmail.com>
2018-03-28 12:37:58 +0300

    refactor(request:aeth): return also when mined

--- main/modules/auth/request-aeth.ts
+++ main/modules/auth/request-aeth.ts
@@ -1,8 +1,9 @@
 import * as Promise from 'bluebird';
-import { post as POST } from 'superagent';
-import { FAUCET_TOKEN, FAUCET_URL } from '../../config/settings';
+import {post as POST} from 'superagent';
+import {FAUCET_TOKEN, FAUCET_URL} from '../../config/settings';
+import {Contracts} from '../../contracts/index';
 
-const execute = Promise.coroutine(function*(data: RequestEtherRequest) {
+const execute = Promise.coroutine(function*(data: RequestEtherRequest, cb) {
     const response = yield Promise.fromCallback(function (cb1) {
         return POST(FAUCET_URL)
             .set('Content-Type', 'application/json')
@@ -14,7 +15,15 @@ const execute = Promise.coroutine(function*(data: RequestEtherRequest) {
         }
         return body;
     });
-    return response ;
+
+    if (!response.tx) {
+        throw new Error('The request could not be completed.');
+    }
+
+    Contracts.watchTx(response.tx)
+        .then(success => cb('', success)).catch(err => cb(err));
+    return response;
+
 });
 
-export default { execute, name: 'requestEther' };
+export default { execute, name: 'requestEther', hasStream: true };
