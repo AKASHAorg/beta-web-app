d7badfd57256332c11f330639b12753d6bf10329
Marius Darila <marius.darila@gmail.com>
2018-03-28 11:46:49 +0300

    fix(request:aeth): remove cb

--- main/modules/auth/request-aeth.ts
+++ main/modules/auth/request-aeth.ts
@@ -2,7 +2,7 @@ import * as Promise from 'bluebird';
 import { post as POST } from 'superagent';
 import { FAUCET_TOKEN, FAUCET_URL } from '../../config/settings';
 
-const execute = Promise.coroutine(function*(data: RequestEtherRequest, cb) {
+const execute = Promise.coroutine(function*(data: RequestEtherRequest) {
     return Promise.fromCallback(function (cb1) {
         return POST(FAUCET_URL)
             .set('Content-Type', 'application/json')
@@ -10,11 +10,10 @@ const execute = Promise.coroutine(function*(data: RequestEtherRequest, cb) {
             .end(cb1);
     }).then((body) => {
         if (body.ok && body.text) {
-            cb('testing faucet');
             return JSON.parse(body.text);
         }
         return body;
     });
 });
 
-export default { execute, name: 'requestEther', hasStream: true };
+export default { execute, name: 'requestEther' };
