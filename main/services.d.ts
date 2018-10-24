dfe28a41ad1a16309c9b266eab10ee1d3335c289
Marius Darila <marius.darila@gmail.com>
2018-10-24 20:35:07 +0300

    refactor(web3): add handler for privacy enabled metamask

--- main/services.d.ts
+++ main/services.d.ts
@@ -1,4 +1,4 @@
-export declare const regenWeb3: () => any;
+export declare const regenWeb3: (web3: any) => any;
 export declare const web3Api: any;
 export declare const ipfsApi: any;
 export declare const ipfsProvider: any;
